7355a7c075f4221440ba5866174849ba11c3a7ef
Aaron Mortenson <trunkatedpig@gmail.com>
2016-03-10 18:16:26 -0500

    frontpage

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
+
+
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    articles=[["Zac Smiles at Camera","http://i.imgur.com/P8nsRbX.jpg"],["Sameer Hits Balls","http://i.imgur.com/y7PQTF7.jpg"],["Darren Throws Balls","http://imgur.com/LiB1npZ.jpg"]]
+    games=[["CMU","BU",10,3,"March 6"],["CMU","Xavier",4,15,"March 7"]]
+    return render_template('index.html', slides=articles, scores=games)
 
 @app.route('/stats')
 def stats():
