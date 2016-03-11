770ea4e3da925fbf0fc78d6ccb6cedf74eb7194b
Aaron Mortenson <trunkatedpig@gmail.com>
2016-03-11 17:19:31 -0500

    push new changes

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ app.debug = True
 @app.route('/home')
 def index():
     articles=[["Zac Smiles at Camera","http://i.imgur.com/P8nsRbX.jpg"],["Sameer Hits Balls","http://i.imgur.com/y7PQTF7.jpg"],["Darren Throws Balls","http://imgur.com/LiB1npZ.jpg"]]
-    games=[["CMU","BU",10,3,"March 6"],["CMU","Xavier",4,15,"March 7"]]
+    games=[["Boyce","CMU",1,14,"October 16"],["CMU","George Mason",4,2,"March 6"],["Boston University","CMU",3,10,"March 6"],["VCU","CMU",14,17,"March 7"],["CMU","Xavier",4,15,"March 7"]]
     return render_template('index.html', slides=articles, scores=games)
 
 @app.route('/stats')
 def stats():
-    return render_template('stats.html')
+    hitters=[["Aaron Mortenson",4,12,10,0,2,0,0,0,1,2,7,0,0,'.200','.333','.200','.533']]
+    pitchers=[]
+    return render_template('stats.html', hitters=hitters, pitchers=pitchers)
 
 @app.route('/schedule')
 def schedule():
@@ -22,7 +24,8 @@ def schedule():
 
 @app.route('/roster')
 def roster():
-    return render_template('roster.html')
+    players = [["Aaron Mortenson","http://i.imgur.com/UR6rM48.jpg",30,"OF","Freshman","R","R",'''6'0"''',"195"]]
+    return render_template('roster.html', players=players)
 
 @app.route('/about')
 def about():
@@ -34,7 +37,8 @@ def news():
 
 @app.route('/upload')
 def upload():
-    return render_template('upload.html')
+    players=["Alex Walenczyk","Morgan Dively","Darren Kerfoot","Zac Ettensohn"]
+    return render_template('upload.html', players=players)
 
 @app.route('/zac')
 def zac():
