58dce3be5c4a562f4a2d6e49f7bd7cc761750c8f
Aaron Mortenson <trunkatedpig@gmail.com>
2016-08-25 21:01:48 -0400

    added continuious data pulling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from get_data import get_site_data
-
+from threading import Timer
 
 app = Flask(__name__)
 app.debug = True
@@ -11,6 +11,7 @@ def load_data():
   Players = raw_data[1]
   Schedule = raw_data[2]
   News = raw_data[3]
+  print raw_data[4]
 
   Articles=[]
   for article in News:
@@ -23,38 +24,7 @@ def load_data():
 #[0:AWAY,1:HOME,2:AWAYSCORE,3:HOMESCORE,4:DATE,5:ID,6:WIN(bool),7:AWAY[R,H,E],8:HOME[R,H,E],9:INNINGS[[A,H],[A,H],...],10:HITTERS[ID,NAME,POS,NUMBER,STATS],11:PITCHERS[ID,NAME,NUMBER,STATS]]
 #STATS:[PA,AB,R,H,2B,3B,HR,RBI,BB,SO,SB,CS]
 #STATS:[W,L,GS,CG,SHO,SV,OUTS,H,R,ER,HR,BB,SO]
-  Games=[["CMU","CCAC Boyce",7,1,"March 26",6,True,[7,10,0],[1,7,1],[[0,0],[1,0],[0,0],[3,0],[0,0],[2,0],[1,1]],
-         [[10,"Drew Fitzmorris","LF","26",4,3,0,1,0,1,0,0,1,1,1,0],
-          [ 1,"Morgan Dively","3B","2"    ,4,3,0,0,0,0,0,0,1,0,0,0],
-          [ 2,"Alex Walenczyk","CF","3"  ,4,4,0,2,1,0,0,0,0,1,0,1],
-          [ 4,"Zachary Ettensohn","1B","9",4,3,1,2,0,0,0,0,1,1,0,0],
-          [18,"Josh Solarek","C","12"    ,4,1,3,0,0,0,0,0,3,0,1,0],
-          [12,"Tahj Spigner","2B","42"   ,4,3,1,2,0,0,0,0,1,0,1,0],
-          [ 8,"Bryant Brackus","DH","23" ,4,2,0,0,0,0,0,2,1,0,0,2],
-          [13,"Jacob Johnson","SS","30"  ,4,3,1,1,0,0,0,1,1,2,1,0],
-          [ 3,"Cameron Dively","RF","6"  ,4,3,1,0,0,0,0,0,1,3,0,0],
-          [ 9,"Nick Halbedl","EH","25"    ,3,3,0,2,0,0,0,2,0,0,0,1]],
-         [[ 0,"Sameer Kolluri","1",1,0,1,1,0,0,21,7,1,1,0,0,4]]],
-       ["CMU","CCAC Boyce",12,1,"March 26",5,True,[12,8,0],[1,4,2],[[1,1],[3,0],[1,0],[1,0],[3,0],[0,0],[3,0]],
-          #ID,NAME,POS,NUMBER             ,P,A,R,H,2,3,4,B,W,K,S,C
-         [[10,"Drew Fitzmorris","LF","26" ,5,3,2,1,0,1,0,1,2,0,1,0],
-          [ 1,"Morgan Dively","3B","2"     ,5,2,0,0,0,0,0,0,3,1,0,0],
-          [ 2,"Alex Walenczyk","CF","3"   ,5,3,1,0,0,0,0,1,1,0,2,0],
-          [ 4,"Zachary Ettensohn","1B","9" ,5,4,0,1,0,0,0,0,1,1,0,0],
-          [18,"Josh Solarek","C","12"     ,4,4,2,2,1,0,0,0,0,0,0,0],
-          [ 9,"Nick Halbedl","RF","25"     ,4,3,0,0,0,0,0,0,1,1,0,0],
-          [12,"Tahj Spigner","2B","42"    ,3,2,1,0,0,0,0,1,1,0,2,0],
-          [20,"Hidetaka Okamura","2B","11",1,0,1,0,0,0,0,0,1,0,0,0],
-          [ 3,"Cameron Dively","EH","6"   ,4,2,1,0,0,0,0,0,2,2,1,0],
-          [ 8,"Bryant Brackus","P","23"   ,4,4,3,3,1,0,0,1,0,0,1,0],
-          [ 0,"Sameer Kolluri","3B","1"   ,4,3,1,1,0,0,0,1,1,0,0,0]],
-         [[ 8,"Bryant Brackus","23",1,0,1,0,0,0,15,2,1,1,0,3,5],
-          [13,"Jacob Johnson","15" ,0,0,0,0,0,0,6 ,2,0,0,0,1,5]]],
-       ["CMU","Xavier",4,15,"March 7",4,False,[0,0,0],[0,0,0],[],[],[]],
-       ["VCU","CMU",14,17,"March 7",3,True,[0,0,0],[0,0,0],[],[],[]],
-       ["Boston University","CMU",3,13,"March 6",2,True,[0,0,0],[0,0,0],[],[],[]],
-       ["CMU","George Mason",4,2,"March 6",1,True,[0,0,0],[0,0,0],[],[],[]]]
-
+  Games = raw_data[4]
 #Hitters=[["Aaron Mortenson",4,12,10,0,2,0,0,0,1,2,7,0,0,'.200','.333','.200','.533']]
   Hitters= [None] * len(Players)
   Pitchers= [None] * len(Players)
@@ -114,7 +84,16 @@ def load_data():
 
   return (Players,Schedule,News,Articles,Playernames,Games,Hitters,Pitchers)
 
-Players,Schedule,News,Articles,Playernames,Games,Hitters,Pitchers = load_data()
+
+def run_forever():
+  global Players,Schedule,News,Articles,Playernames,Games,Hitters,Pitchers
+  try:
+    Players,Schedule,News,Articles,Playernames,Games,Hitters,Pitchers = load_data()
+  except:
+    pass
+  Timer(300.0, run_forever).start()
+
+run_forever()
 
 def gameInfo(id):
   for game in Games:
--- get_data.py
+++ get_data.py
@@ -15,7 +15,6 @@ def get_spreadsheet(key):
       processed.append(row)
     return processed
 
-  print map(get_data, feed.entry)
   return map(get_data, feed.entry)
 
 def get_site_data(key):
@@ -26,12 +25,54 @@ def get_site_data(key):
       return height[1:-2]
     return height
 
+  def process_game(game):
+    basic_info = game[1][:7]
+
+    basic_info[5] = int(basic_info[5])
+    if basic_info[6] == "TRUE":
+      basic_info[6] = True
+    else:
+      basic_info[6] = False
+
+    game_info = basic_info
+
+    score_info = game[4:6]
+    away_score_info = [elem for elem in score_info[0][1:] if elem != ""]
+    home_score_info = [elem for elem in score_info[1][1:] if elem != ""]
+    if len(away_score_info) >= 3 and len(home_score_info) >= 3:
+      game_info.append(away_score_info[-3:])
+      game_info.append(home_score_info[-3:])
+      line_score_info = []
+      for i in range (len(away_score_info) - 3):
+        if i < len(home_score_info):
+          line_score_info.append([away_score_info[i],home_score_info[i]])
+        else:
+          line_score_info.append([away_score_info[i],0])
+      game_info.append(line_score_info)
+    else:
+      game_info.append([0,0,0])
+      game_info.append([0,0,0])
+      game_info.append([])
+
+
+    game_stats = game[8:]
+    divider = next((i for i, item in enumerate(game_stats) if not item[0].isdigit()), -1)
+    #functional programming has ruined my life
+    hitter_stats = map(lambda info: map(lambda element: int(element) if element.isdigit() else element, info),
+                       filter(lambda info: info[0].isdigit(), game_stats[:divider]))
+    pitcher_stats = map(lambda info: map(lambda element: int(element) if element.isdigit() else element, info),
+                        filter(lambda info: info[0].isdigit(), game_stats[(divider + 1):]))
+
+    game_info.append(hitter_stats)
+    game_info.append(pitcher_stats)
+
+    return game_info
+
   raw_data[1] = map(lambda info: info[:7] + [process_heights(info[7]), info[8], int(info[9])], raw_data[1][1:])
   raw_data[2] = map(lambda info: info[:5] + [int(info[5])], raw_data[2][1:])
   raw_data[3] = map(lambda info: info[:4] + [info[4].split('\t')], raw_data[3][1:])[::-1]
-  raw_data[4] = raw_data[4:]
+  raw_data[4] = map(process_game, raw_data[4:])
 
   raw_data = raw_data[:5]
 
   return raw_data
-
